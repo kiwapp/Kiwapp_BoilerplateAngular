9fc58938241a7a0889523d0f500992b628a2eae9
Guillaume Roussel <guillaume@procheo.fr>
2014-11-18 17:59:56 +0100

    Update index.js

--- app/tasks/index.js
+++ app/tasks/index.js
@@ -4,6 +4,6 @@ var gulp   = require('gulp');
  * Simply move the index.html file into the build folder
  */
 module.exports = function() {
-    return gulp.src('./src/*')
+    return gulp.src('./src/**.*')
         .pipe(gulp.dest('./build/'));
 };

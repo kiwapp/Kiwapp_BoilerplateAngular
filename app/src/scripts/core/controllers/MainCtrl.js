b9a4ec2cdaec2744c1b5abd8df5637a6de403c9f
Aldorus <guillaume@procheo.fr>
2014-11-18 10:50:24 +0100

    remove useless files

--- app/src/scripts/core/controllers/MainCtrl.js
+++ app/src/scripts/core/controllers/MainCtrl.js
@@ -1,51 +1,5 @@
 'use strict';
 /*@ngInject*/
-module.exports = function ($scope, $rootScope, $modal) {
+module.exports = function ($scope, $rootScope) {
 
-    // Read the widgets list for the home page
-    $scope.blocs = getBlocsList('page-home');
-
-    /**
-     * Get the list of all blocs by page (retrived from the config.js)
-     * @param string page
-     * @returns array
-     */
-    function getBlocsList (page) {
-        return Kiwapp.get('instanceParameters')['app-params'][page]
-    }
-
-    /**
-     * EVENTS WIDGETS
-     */
-    // Listen Event form widget (this event is trigger by : simple-link-widget)
-    $rootScope.$on('openPage', function(event, params){
-        console.log("OpenPage");
-        $scope.openPage = true;
-        $modal.open({
-            templateUrl: 'core/partials/modal-page.html',
-            controller: 'PageModalController',
-            windowClass: 'modal-page',
-            backdrop: false,
-            resolve: {
-                blocs: function () {
-                    console.log(params.url);
-                    console.log(getBlocsList(params.url));
-                    return getBlocsList(params.url);
-                }
-            }
-        });
-    });
-
-    $rootScope.$on('openUrl', function(event, params){
-        // TODO
-
-    });
-
-    $rootScope.$on('openNativeApp', function(event, params){
-        // TODO
-    });
-
-    $rootScope.$on('openHTML5App', function(event, params){
-        // TODO
-    });
 };
--- app/tasks/init.js
+++ app/tasks/init.js
@@ -23,34 +23,48 @@ module.exports = function () {
         });
     }
 
+    function replacement() {
+        gulp.src("./src/index.html")
+            .pipe(
+            prompt.prompt([
+                    {
+                        type: 'input',
+                        name: 'displayName',
+                        message: 'Choose a name for your application : '
+                    },
+                    {
+                        type: 'input',
+                        name: 'desciption',
+                        message: '(Optional) Insert a description for your application here : '
+                    }
+                ], function (res) {
+                    console.log("Project initialization please wait a second...");
+                    // Callback method
+                    gulp.src(
+                        [
+                            './**/*.json',
+                            './**/*.js',
+                            './**/*.html',
+                            './**/*.md',
+                            '!./tasks/init.js',
+                            '!./node_modules/**/*',
+                            '!./src/vendor/**/*'
+                        ])
+                        .pipe(replace('<%%=displayName%>', res.displayName))
+                        .pipe(replace('<%%=applicationName%>', camelCase(res.displayName)))
+                        .pipe(replace('<%%=displayName%>', res.desciption))
+                        .pipe(gulp.dest('./'));
+                }
+            ));
+    }
+
     // Install your bower packages
     bower.commands.install([], {save: true}, {})
         .on('end', function (installed) {
             console.log("All package are up to date");
+            replacement();
         });
 
 
-    gulp.src("./src/index.html")
-        .pipe(
-        prompt.prompt([
-                {
-                    type: 'input',
-                    name: 'displayName',
-                    message: 'Choose a name for your application : '
-                },
-                {
-                    type: 'input',
-                    name: 'desciption',
-                    message: '(Optional) Insert a description for your application here : '
-                }
-            ], function (res) {
-                console.log("Project initialization please wait a second...");
-                // Callback method
-                gulp.src(['./**/*', '!./tasks/init.js','!./node_modules/**/*', '!./src/vendor/**/*'])
-                    .pipe(replace('<%%=displayName%>', res.displayName))
-                    .pipe(replace('<%%=applicationName%>', camelCase(res.displayName)))
-                    .pipe(replace('<%%=displayName%>', res.desciption))
-                    .pipe(gulp.dest('./'));
-            }
-        ));
+
 };

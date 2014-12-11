52f2b6242db2e5435d3a8877e3677f62fa1f550b
Aldorus <guillaume@procheo.fr>
2014-12-11 15:25:20 +0100

    CUST-129 replace the templating in task template

--- app/tasks/templates.js
+++ app/tasks/templates.js
@@ -10,7 +10,7 @@ module.exports = function() {
     return gulp.src('./src/scripts/**/*.html')
         .pipe(htmlify())
         .pipe(templateCache('templates.js',{
-            module: 'test'
+            module: '<%%=applicationName%>'
         }))
         .pipe(gulp.dest(config.dist + 'js'));
 }

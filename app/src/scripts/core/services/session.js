6f4149b90109c195b1d69d43e29f4123ad856316
Aldorus <guillaume@procheo.fr>
2015-06-25 15:35:59 +0200

    Remove DS Store with ignore and add session timeout gestion

--- app/src/scripts/core/services/session.js
+++ app/src/scripts/core/services/session.js
@@ -6,21 +6,53 @@
 module.exports = function (TIMEOUT) {
     var service = {};
 
+    service.waiting = true;
+
+    function launchTimeout(callback) {
+        // Launch the timeout
+        Kiwapp.session().launchTimeout(function (time) {
+
+            Kiwapp.session().end().start();
+
+            // If the application is not in waiting mode we send the end event stats
+            if(!service.waiting) {
+                Kiwapp.log('End user session');
+                // We launch a new session event stat
+                Kiwapp.stats().event('Fin de session', {sessionDuration: time});
+                callback();
+            }
+
+            service.waiting = true;
+
+        }, SESSION_TIMEOUT, true);
+    }
+
     service.init = function init(callback) {
+        // Launch the session interaction
+        Kiwapp.session().end().start();
 
+        launchTimeout(callback);
 
-        // Init the timeout on touch event
         document.body.addEventListener('touchstart', function () {
             // Stop the timeout
             Kiwapp.session().resetTimeout();
+            if(service.waiting) {
+                // The application was in waiting mode (no user)
+                // We launch a new session event stat
+                Kiwapp.log('Start user session');
+                Kiwapp.stats().event('Début de session');
+                // We aren't in waiting mode anymore
+                service.waiting = false;
+            }
         });
+
         document.body.addEventListener('touchend', function () {
-            // Launch the timeout
-            Kiwapp.session().launchTimeout(function () {
-                Kiwapp.session().end().start();
-                callback();
-            }, TIMEOUT);
+            launchTimeout(callback);
         });
+    };
+
+    service.stopTimeout = function stopTimeout() {
+        Kiwapp.session().resetTimeout();
     };
 
     return service;
